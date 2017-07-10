cdaf6aeec3348fc1e8c01a36eb8cfac7e5df18b7
Iarmoliuk Taras <iarmoliuktaras@gmail.com>
2017-07-10 12:35:21 +0300

    test

--- MyTwitter/HomeDatasourceController.swift
+++ MyTwitter/HomeDatasourceController.swift
@@ -19,6 +19,8 @@ class HomeDatasourceController: DatasourceController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        
+        
         collectionView?.backgroundColor = UIColor(r: 232, g: 236, b: 241)
         setupNavigationBarItems()
         
